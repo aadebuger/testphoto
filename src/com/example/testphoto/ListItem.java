1c274fa1e3de0d73b9c79c7c20f560d8096e9a95
aadebuger <admin@local.host>
2014-12-09 18:02:39 +0800

    first commit

---
+++ src/com/example/testphoto/ListItem.java
@@ -0,0 +1,29 @@
+package com.example.testphoto;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class ListItem extends LinearLayout {
+
+	
+	
+	public ListItem(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initView(context);
+	}
+
+	public ListItem(Context context) {
+		super(context);
+		initView(context);
+	}
+	
+	private void initView(Context context) {
+		View view = LayoutInflater.from(context).inflate(R.layout.list_item, null);
+		
+		addView(view);
+	}
+	
+}
